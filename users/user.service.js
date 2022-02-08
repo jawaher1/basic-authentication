cc8de2b59eb9538083a280cd5b6f7e46c06b9182
Jawaher Mairech <jawaher@fingenesis.com>
2022-02-08 11:32:06 +0100

    basic authentication

---
+++ users/user.service.js
@@ -0,0 +1,15 @@
+﻿const users = [{ id: 1, username: 'Xusername', password: 'xpass', firstName: 'XUser', lastName: 'YUser' }];
+
+module.exports = {
+    authenticate,
+};
+
+async function authenticate({ username, password }) {
+    const user = users.find(u => u.username === username && u.password === password);
+    if (user) {
+        const { password, ...userWithoutPassword } = user;
+        return userWithoutPassword;
+    }
+}
+
+
